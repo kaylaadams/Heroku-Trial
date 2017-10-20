30a4c0a4289b85399ddd5bfeb44be26059aad93e
Kayla Danielle Adams <kayladanielleadams@Kaylas-MacBook-Pro.local>
2017-10-20 15:49:00 -0500

    Prepared for Heroku deployment

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,6 @@ app.use(express.static(clientPath));
 
 console.log(process.env.GREETING);
 
-app.listen(3000,  () => {
+app.listen(process.env.PORT || 3000,  () => {
     console.log('listening on port 3000');
 });
